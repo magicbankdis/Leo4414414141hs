3c0599a478e6d454ae892ec79932968495a1e73b
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-08-11 10:10:29 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,16 +18,18 @@ message.channel.sendEmbed(embed);
 });
 
 client.on("message", message => {
+    var prefix = "!"
     if (!message.content.startsWith(prefix)) return;
       let command = message.content.split(" ")[0];
       command = command.slice(prefix.length);
-        if("--SPskin") {
+        if(command === "skin") {
                 const args = message.content.split(" ").slice(1).join(" ")
         if (!args) return message.channel.send("** Type your skin name **");
         const image = new Discord.Attachment(`https://visage.surgeplay.com/full/256/${args}`, "skin.png");
     message.channel.send(image)
         }
     });
+
 
 
 
