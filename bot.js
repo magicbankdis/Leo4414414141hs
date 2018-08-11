faf1699d01e3417e7465137f5b42a27cea70f7e9
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-08-11 09:59:08 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,26 @@
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = '--SP';
+client.on('ready', () => {
+  client.user.setActivity("--SPhelp",{type: 'WATCHING'})
+
+});
+
+client.on('message', message => {
+    if (message.content.startsWith("--SAhelp")) {
+let embed = new Discord.RichEmbed()
+.setThumbnail(message.author.avatarURL)
+.addField('     **GOMING SOON** ' ,' **SpeedPermium** ')
+
+
+
+
+
+.setColor('#24efbd')
+message.channel.sendEmbed(embed);
+}
+});
+
+
+client.login(process.env.BOT_TOKEN2);
