6e9efc20c46ed5e79eb55d1698ca12f169af670b
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-08-18 14:27:30 +0100

    Update bot2.js

--- bot2.js
+++ bot2.js
@@ -98,13 +98,13 @@ client.user.setGame(argresult);
 } else
 
 if (message.content.startsWith(prefix + 'setname')) {
-  if (message.author.id !== '337629134371160065') return message.reply('**only for the botowner **')
+  if (message.author.id !== '471788143637430273') return message.reply('**only for the botowner **')
   client.user.setUsername(argresult).then
       message.channel.sendMessage(`**${argresult}** : done`)
 } else
 
 if (message.content.startsWith(prefix + 'stream')) {
-  if (message.author.id !== '337629134371160065') return message.reply('**only for the botowner **')
+  if (message.author.id !== '471788143637430273') return message.reply('**only for the botowner **')
 client.user.setGame(argresult, "https://www.twitch.tv/0rzh0");
     message.channel.sendMessage(`**${argresult}** streem changed to`)
     message.react("ðŸ‘Œ")
@@ -112,7 +112,7 @@ client.user.setGame(argresult, "https://www.twitch.tv/0rzh0");
 } else
 
 if (message.content.startsWith(prefix + 'watching')) {
-  if (message.author.id !== '337629134371160065') return message.reply('**only for the botowner **')
+  if (message.author.id !== '471788143637430273') return message.reply('**only for the botowner **')
     client.user.setActivity(argresult, {type : 'watching'});
  message.channel.sendMessage(`**${argresult}** : Watching changed to`)
  message.react("ðŸ‘Œ")
@@ -120,7 +120,7 @@ if (message.content.startsWith(prefix + 'watching')) {
 } else
 	
 if (message.content.startsWith(prefix + 'listening')) {
-  if (message.author.id !== '337629134371160065') return message.reply('**only for the botowner **')
+  if (message.author.id !== '471788143637430273') return message.reply('**only for the botowner **')
 client.user.setActivity(argresult);
     message.channel.sendMessage(`**${argresult}** : listening changed to`)
     message.react("ðŸ‘Œ")
@@ -128,7 +128,7 @@ client.user.setActivity(argresult);
 } else
 
 if (message.content.startsWith(prefix + 'setavatar')) {
-if (message.author.id !== '337629134371160065') return message.reply('**only for the botowner **')
+if (message.author.id !== '471788143637430273') return message.reply('**only for the botowner **')
 client.user.setAvatar(argresult);
     message.channel.sendMessage(`**${argresult}** :avatar changed to`);
 	message.react("ðŸ‘Œ")
@@ -144,14 +144,14 @@ client.user.setAvatar(argresult);
 
 //client.on('ready', () => {
    // setInterval(function(){
-  //      client.guilds.get('328932248109842433').roles.find('name', 'big boos').edit({color: 'RANDOM'})
+  //      client.guilds.get('471788143637430273').roles.find('name', 'big boos').edit({color: 'RANDOM'})
  //   },1);
 //	});
 
 
 //client.on('ready', () => {
  //   setInterval(function(){
-      //  client.guilds.get('328932248109842433').roles.find('name', '⚔ ≺Captain≻⚔').edit({color: 'RANDOM'})
+      //  client.guilds.get('471788143637430273').roles.find('name', '⚔ ≺Captain≻⚔').edit({color: 'RANDOM'})
  //   },1);
 //	});
 
